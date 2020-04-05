561e00ff3cd18328dfbc0443a4dfbfa2007fb08f
jeffreyadamo <58490053+jeffreyadamo@users.noreply.github.com>
2020-04-04 20:40:28 -0700

    final commit: Local Storage changes, README.md updated, cleaned up extraneous code

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# 05_3rdPartyAPIs_WorkDayScheduler
-Homework for Week 5
 # 05 Third-Party APIs: Work Day Scheduler
 
+## Objective:
 Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
 You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
@@ -14,25 +13,148 @@ I WANT to add important events to a daily planner
 SO THAT I can manage my time effectively
 ```
 
-## Acceptance Criteria
+## Acceptance Criteria (with notes - more notes in script.js)
 
-```
+
 GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
 THEN the current day is displayed at the top of the calendar
+```javascript
+//Utilizing moment.js CDN, variables were set and placed into the #currentDay <div>:
+    var toDay = moment().format('dddd'); 
+    var currentDate = moment().format('MMMM Do');
+
+    $("#currentDay").html(toDay + ", " + currentDate + "<br><br>");
+```
 WHEN I scroll down
 THEN I am presented with timeblocks for standard business hours
+```javascript
+//Use a for loop to add rows for each timeBlock. Make use of variables and insert the attributes in order to not get confused or too wordy. 
+
+    for(var i=0; i<timesOfday.length; i++){
+    //Insert a new div containing a row for each timeBlock
+    var row = $("<div>");
+    row.attr("class", "row");
+    row.attr("data-hour",timesOfday[i]);
+
+    //Insert 3 columns with the hour, description, and save button
+    var col1= $("<div>");
+    col1.attr("class","col-md-2 hour");
+    col1.text(timesOfday[i]);
+    
+    var col2BootStrap = $("<div>");
+    col2BootStrap.addClass("col-md-8 form-group description");
+    col2textArea = $('<textarea class="form-control" rows="3"></textarea>');
+    col2textArea.addClass(timesOfday[i]+"color");
+    col2textArea.attr("data-hour",timesOfday[i]);
+    col2BootStrap.append(col2textArea);
+
+    var col3=$("<button>");
+    col3.addClass("col-md-2 fas fa-save saveBtn");
+    col3.attr("data-hour",timesOfday[i]);
+
+    //Append each colum to the row and then append the row to the timeBlock in the HTML
+    row.append(col1, col2BootStrap, col3);
+    $(".time-block").append(row);
+}
+```
+* I couldn't figure out how to assign the hover over save button style changed here.
+
 WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+```javascript
+    //Use a for loop to compare the data-hour of each timeBlock to the currentTime. I've made sure these are both strings a lot using "typeof".
+    for (var a=0; a<colorOfBlocks.length; a++){
+
+        var colorBlock = colorOfBlocks[a];
+        console.log("var colorBlock ="+colorBlock);
+        console.log("var colorBlock is a "+typeof colorBlock);
+        console.log($('.'+colorBlock+'').data('hour'));
+
+        var colorofBlocksdata = $("."+colorOfBlocks[a]+"").data("hour");
+        console.log(colorofBlocksdata);
+
+        //Start with all of them setup as gray
+        $('.'+colorOfBlocks[a]+'').addClass("past");
+
+        //Setup conditional if/else statements to assign color to class
+
+        if (colorofBlocksdata > currentTime){
+            $('.'+colorOfBlocks[a]+'').addClass("future");
+            console.log("color should be green");
+        } else if (colorofBlocksdata < currentTime) {
+            $('.'+colorOfBlocks[a]+'').addClass("past");
+            console.log("color should be gray");
+        } else if (colorofBlocksdata === currentTime){
+            $('.'+colorOfBlocks[a]+'').addClass("present");
+            console.log("color should be red");
+        } else{
+            
+        } 
+    }
+```
+* I struggled to get this to work correctly. It seems to want to set the first timeBlock as future and will be green when it is not 9AM. I switched between using strings and numbers and went with the final string output of the "data-hour" of the timeBlock, which seems to be the format from moment.js
+* I spent a long time on this.
+
+
 WHEN I click into a timeblock
 THEN I can enter an event
 WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
+```javascript
+    //Set variables we are going to save and get outside of the onclick:
+    var readyToSave = [];
+    var storedScores = [];
+    var storeHour;
+    var storeInput;
+    var storedData;
+
+    $(".saveBtn").on("click", function () {
+        //Ensure when I click the save it will know which button is clicked
+        var clickLocation = $(this).data('hour');
+        console.log('clicked on ' + clickLocation);
+
+        //Let's save data: create variables for the hour & input(value)
+        storeHour = $(this).data("hour")+"color"
+        console.log(storeHour);
+        storeInput = $("."+storeHour+"").val();
+        console.log(storeInput);
+
+        storedData = {storeHour, storeInput};
+        readyToSave.push(storedData);
+
+        localStorage.setItem("timeClass,text", JSON.stringify(readyToSave));
+        loadData();
+    });
 ```
 
-The following animation demonstrates the application functionality:
+WHEN I refresh the page
+THEN the saved events persist
+```javascript
+ //Let's load this:
+    function loadData (){
+    if (localStorage.getItem("timeClass,text") !== null) {
+        storedScores = JSON.parse(localStorage.getItem("timeClass,text"));
+        console.log(storedScores);
+        console.log(storedScores.storeHour);
+        console.log(storedScores.storeInput);
+
+        for (var b = 0; b < storedScores.length; b++){
+            console.log(storedScores[b].storeHour);
+            console.log(storedScores[b].storeInput);
+            $('.'+storedScores[b].storeHour+'').val(storedScores[b].storeInput);
+        }
+    }}
+
+    loadData();
+```
+* I struggled with this part. The bug here is upon refreshing, everything is still there (yay!), but when hitting a save button, it erases everything except for the last entry. 
+
+## Fin.
+
+The following animation demonstrates what the application should be doing:
+
+
 
 ![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
 
--- script.js
+++ script.js
@@ -1,125 +1,149 @@
-// Set some time variable
+// Set some time variables taken from moment.js cdn
 var toDay = moment().format('dddd');  
 var currentDate = moment().format('MMMM Do');
 var currentTime = moment().format('LT');
 var currentHour = parseInt(moment().format('h'));
 
-//working on military time to make later if/then class assignments
-if (currentHour === 1 ||currentHour === 2 ||currentHour === 3 ||currentHour === 3 ||currentHour === 5 ){
-    currentHour = currentHour + 12
-};
-// var currentHour = JSON.stringify(currentHour);
-
-
 //spot check
+if (currentTime < "7PM"){
+    console.log("I can read time by string")
+}
 console.log("The current time is: " +currentTime);
 console.log("currentTime is a: " + typeof currentTime);
-console.log("The currentHour is " + currentHour);
-console.log("currentHour is a: " + typeof currentHour);
 
 
-// insert Date
+// Insert Date (and time)
 $("#currentDay").html(toDay + ", " + currentDate + "<br><br>");
 $("#currentDay").append("It is currently " + currentTime);
 
-// change this to a for loop to append all times from 9-5
-var timeSlot = [9 ,10,11,12,13,14,15,16,17];
-var timesOfday=[]
-for (var j=0; j<9; j++){
-    timesOfday.push("hour"+j);
-}
-console.log(timeSlot);
-console.log(timeSlot[1]);
+// Set an array up to create the desired timeBlocks for 
+var timesOfday=["9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
 //Use a for loop to add rows for each timeBlock. Make use of variables and insert the attributes in order to not get confused or too wordy. 
 
-for(var i=0; i<timeSlot.length; i++){
-    var row = $("<div>")
-    row.attr("class", "row")
-    var col1= $("<div>")
-    col1.attr("class","col-md-2 hour" )
-    col1.text(timeSlot[i])
-    
+for(var i=0; i<timesOfday.length; i++){
+    //Insert a new div containing a row for each timeBlock
+    var row = $("<div>");
+    row.attr("class", "row");
+    row.attr("data-hour",timesOfday[i]);
 
+    //Insert 3 columns with the hour, description, and save button
+    var col1= $("<div>");
+    col1.attr("class","col-md-2 hour");
+    col1.text(timesOfday[i]);
+    
     var col2BootStrap = $("<div>");
     col2BootStrap.addClass("col-md-8 form-group description");
-    col2textArea = $('<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>')
-    col2textArea.addClass("timeColor"+timeSlot[i])
-    col2textArea.attr("data-hour",timeSlot[i]);
+    col2textArea = $('<textarea class="form-control" rows="3"></textarea>');
+    col2textArea.addClass(timesOfday[i]+"color");
+    col2textArea.attr("data-hour",timesOfday[i]);
     col2BootStrap.append(col2textArea);
 
+    var col3=$("<button>");
+    col3.addClass("col-md-2 fas fa-save saveBtn");
+    col3.attr("data-hour",timesOfday[i]);
 
-    var col3=$("<button>")
-    col3.addClass("col-md-2 fas fa-save saveBtn")
-    row.append(col1, col2BootStrap, col3)
-    $(".time-block").append(row)
-
+    //Append each colum to the row and then append the row to the timeBlock in the HTML
+    row.append(col1, col2BootStrap, col3);
+    $(".time-block").append(row);
 }
 
-// timeColor# is the class where data-hour is assigned
-var testColor9 = $(".timeColor9");
+//data-hour is assigned in each row and column
+var colorLocation9AM = $(".9AMcolor");
 
 //I can access the data-hour out of the timeColor# class by $.data("hour")
+console.log(colorLocation9AM.data('hour')); //should read 9AM
 
-console.log($(".timeColor9").data("hour"));
-testColor9data = testColor9.data("hour");
+//what kind of data are we working with?
 
-console.log(testColor9data);
+var data9AM = colorLocation9AM.data('hour')
+console.log("data-hour is stored as a " + typeof data9AM); //string
 
-console.log(".timeColor"+timeSlot[0])
-// if (currentHour === testColor9data){
-//     testColor9.addClass("present")
-//     console.log("color should be red")
-// } else if (currentHour < testColor9data){
-//     testColor9.addClass("future")
-//     console.log("color should be green")
-// } else {
-//     testColor9.addClass("past")
-//     console.log("color should be grey")
-// }
+//what kind of data are we pulling from moment.js?
+console.log("currentTime is stored as a " + typeof currentTime); //string
 
-for (a=0; a<timeSlot.length; a++){
-    if (currentHour === $(".timeColor"+timeSlot[a]).data("hour")){
-        $(".timeColor"+timeSlot[a]).addClass("present")
-        console.log("color should be red")
-    } else if (currentHour < $(".timeColor"+timeSlot[a]).data("hour")){
-        $(".timeColor"+timeSlot[a]).addClass("future")
-        console.log("color should be green")
-    } else {
-        $(".timeColor"+timeSlot[a]).addClass("past")
-        console.log("color should be grey")
-    }
+var currentTime = JSON.stringify(currentTime);
+
+//Change the color of the blocks according to their time: I've tried this many ways. Here is the latest.
+
+//set an array to the classnames of the timeBlocks
+var colorOfBlocks = ["9AMcolor", "10AMcolor", "11AMcolor", "12AMcolor", "1PMcolor", "2PMcolor", "3PMcolor", "4PMcolor", "5PMcolor"]
+
+//Use a for loop to compare the data-hour of each timeBlock to the currentTime. I've made sure these are both strings a lot using "typeof".
+for (var a=0; a<colorOfBlocks.length; a++){
+
+    var colorBlock = colorOfBlocks[a];
+    console.log("var colorBlock ="+colorBlock);
+    console.log("var colorBlock is a "+typeof colorBlock);
+    console.log($('.'+colorBlock+'').data('hour'));
+
+    var colorofBlocksdata = $("."+colorOfBlocks[a]+"").data("hour");
+    console.log(colorofBlocksdata);
+
+    //Start with all of them setup as gray
+    $('.'+colorOfBlocks[a]+'').addClass("past");
+
+    //Setup conditional if/else statements to assign color to class
+
+    if (colorofBlocksdata > currentTime){
+        $('.'+colorOfBlocks[a]+'').addClass("future");
+         console.log("color should be green");
+     } else if (colorofBlocksdata < currentTime) {
+         $('.'+colorOfBlocks[a]+'').addClass("past");
+         console.log("color should be gray");
+     } else if (colorofBlocksdata === currentTime){
+         $('.'+colorOfBlocks[a]+'').addClass("present");
+         console.log("color should be red");
+     } else{
+         
+     } 
 }
 
+console.log("-------------------");
+
+//Set variables we are going to save and get outside of the onclick:
+var readyToSave = [];
+var storedScores = [];
+var storeHour;
+var storeInput;
+var storedData;
+
+$(".saveBtn").on("click", function () {
+    //Ensure when I click the save it will know which button is clicked
+    var clickLocation = $(this).data('hour');
+    console.log('clicked on ' + clickLocation);
+
+    //Let's save data: create variables for the hour & input(value)
+    storeHour = $(this).data("hour")+"color"
+    console.log(storeHour);
+    storeInput = $("."+storeHour+"").val();
+    console.log(storeInput);
+
+     storedData = {storeHour, storeInput};
+     readyToSave.push(storedData);
+
+    localStorage.setItem("timeClass,text", JSON.stringify(readyToSave));
+    loadData();
+});
+
+    //Let's load this:
+    function loadData (){
+    if (localStorage.getItem("timeClass,text") !== null) {
+        storedScores = JSON.parse(localStorage.getItem("timeClass,text"));
+        console.log(storedScores);
+        console.log(storedScores.storeHour);
+        console.log(storedScores.storeInput);
+
+        for (var b = 0; b < storedScores.length; b++){
+            console.log(storedScores[b].storeHour);
+            console.log(storedScores[b].storeInput);
+            $('.'+storedScores[b].storeHour+'').val(storedScores[b].storeInput);
+        }
+    }}
+
+    loadData();
 
 
-// $('[data-hour]').each(function() {
-//     console.log($(this).data('hour'));
-//   })
-
-//   $('.timeColor9').each(function() {
-//     console.log($(this).data('hour'));
-//   })
-
-// for (var a=0; a<timeSlot.length; i++){
-//     if ()
-// }
 
 
-//   var timeValue9 = $('.timeColor9').each(function() {
-//     JSON.stringify($(this).data('hour'));
-//   })
-// console.log(timeValue9);
 
-// if (currentHour === $(".data-hour"[3])) {
-// $(".description").addClass(".present");
-// }
-
-// if (currentHour > )
-
-// Write a statement to set an attribute depending on the currentTime
-// if (currentTime === )
-
-
-// $(".hour5").attr("value", "5");
-// console.log($(".hour5").value);
